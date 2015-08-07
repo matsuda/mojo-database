30d6f214927f171fd404cc70d32b426116a4223a
Kosuke Matsuda <matsuda@1020409.local>
2015-08-07 11:39:04 +0900

    MojoModelのテーブル-クラス間命名変換バグ（対応漏れ）の修正
    変換ルールをループするときに特異なものから判定するように、
    ループの順番を保証する。

--- MojoDatabase/NSString+MojoDatabase.m
+++ MojoDatabase/NSString+MojoDatabase.m
@@ -10,47 +10,56 @@
 
 @implementation NSString (MojoDatabase)
 
++ (NSArray *)plurals
+{
+    static dispatch_once_t onceToken;
+    static NSArray *_plurals = nil;
+    dispatch_once(&onceToken, ^{
+        _plurals = [@[
+                      @[@"(quiz)$"                   ,   @"$1zes"],
+                      @[@"^(ox)$"                    ,   @"$1en"],
+                      @[@"([m|l])ouse$"              ,   @"$1ice"],
+                      @[@"(matr|vert|ind)(?:ix|ex)$" ,   @"$1ices"],
+                      @[@"(x|ch|ss|sh)$"             ,   @"$1es"],
+                      @[@"([^aeiouy]|qu)y$"          ,   @"$1ies"],
+                      @[@"(hive)$"                   ,   @"$1s"],
+                      @[@"(?:([^f])fe|([lr])f)$"     ,   @"$1$2ves"],
+                      @[@"sis$"                      ,   @"ses"],
+                      @[@"([ti])um$"                 ,   @"$1a"],
+                      @[@"(buffal|tomat)o$"          ,   @"$1oes"],
+                      @[@"(bu)s$"                    ,   @"$1ses"],
+                      @[@"(alias|status)$"           ,   @"$1es"],
+                      @[@"(octop|vir)us$"            ,   @"$1i"],
+                      @[@"(ax|test)is$"              ,   @"$1es"],
+                      @[@"s$"                        ,   @"s"],
+                      @[@"$"                         ,   @"s"],
+                      ] retain];
+    });
+    return _plurals;
+}
+
 // 文字列中の単語を複数形に変換します
-- (NSString *)pluralizeString {
-    NSDictionary *plurals  = [NSDictionary dictionaryWithObjectsAndKeys:
-                              @"s"       , @"$"                         ,
-                              @"s"       , @"s$"                        ,
-                              @"$1es"    , @"(ax|test)is$"              ,
-                              @"$1i"     , @"(octop|vir)us$"            ,
-                              @"$1es"    , @"(alias|status)$"           ,
-                              @"$1ses"   , @"(bu)s$"                    ,
-                              @"$1oes"   , @"(buffal|tomat)o$"          ,
-                              @"$1a"     , @"([ti])um$"                 ,
-                              @"ses"     , @"sis$"                      ,
-                              @"$1$2ves" , @"(?:([^f])fe|([lr])f)$"     ,
-                              @"$1s"     , @"(hive)$"                   ,
-                              @"$1ies"   , @"([^aeiouy]|qu)y$"          ,
-                              @"$1es"    , @"(x|ch|ss|sh)$"             ,
-                              @"$1ices"  , @"(matr|vert|ind)(?:ix|ex)$" ,
-                              @"$1ice"   , @"([m|l])ouse$"              ,
-                              @"$1en"    , @"^(ox)$"                    ,
-                              @"$1zes"   , @"(quiz)$",
-                              nil];
-    NSArray *keys = [plurals allKeys];
-
-    NSEnumerator *enumerator = [keys objectEnumerator];
-    NSString *replaceStr = nil;
-    NSRegularExpressionOptions options = 0;
-
-    for(NSString *key in enumerator) {
-        NSError *error = nil;
-        NSRegularExpression *regexp = [NSRegularExpression regularExpressionWithPattern:key options:options error:&error];
-        if (error != nil) {
-            NSLog(@"%@", error);
-        } else {
-            NSTextCheckingResult *match = [regexp firstMatchInString:self options:options range:NSMakeRange(0, [self length])];
-            if ([match numberOfRanges] > 0) {
-                replaceStr = [regexp stringByReplacingMatchesInString:self options:options range:NSMakeRange(0, [self length]) withTemplate:[plurals objectForKey:key]];
+- (NSString *)pluralizeString
+{
+    NSString *result = [self copy];
+    @autoreleasepool {
+        NSArray *plurals = [[self class] plurals];
+        for (NSArray *rules in plurals) {
+            NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:rules[0]
+                                                                                   options:NSRegularExpressionCaseInsensitive
+                                                                                     error:nil];
+            NSString *s = [regex stringByReplacingMatchesInString:self
+                                                          options:NSMatchingReportCompletion
+                                                            range:NSMakeRange(0, self.length)
+                                                     withTemplate:rules[1]];
+            if (![self isEqualToString:s]) {
+                [result release];
+                result = [s retain];
+                break;
             }
         }
-        // if (replaceStr != nil && [replaceStr length] > 0) break;
     }
-    return replaceStr;
+    return [result autorelease];
 }
 
 @end
